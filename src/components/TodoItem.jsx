78cc38898387d597cb29db1abafdf15955e36f4d
Cristofher Rojas <cristofher.rojas@inacapmail.cl>
2022-07-06 17:38:47 -0400

    Final

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,19 @@
 import React, { Fragment } from "react";
 
-export function TodoItem({ todo }) {
-  const { id, task } = todo;
+export function TodoItem({ todo, cambiarEstado }) {
+  const { id, task, completed } = todo;
+
+  const fnCambiarEstado = () => {
+    cambiarEstado(id);
+  };
+
   return (
     <Fragment>
-      <li className="list-group-item">{todo.task}</li>
+      <li className="list-group-item">
+        <input type="checkbox" className="form-check-input me-2" onChange={fnCambiarEstado} checked={completed} />
+
+        {todo.task}
+      </li>
     </Fragment>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,28 @@
-import React, { Fragment, useState, useRef } from "react";
+import React, { Fragment, useState, useRef, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import { TodoItem } from "./TodoItem";
 
+const KEY = "todolist-todos";
+
 export function TodoList() {
-  const [todos, setTodos] = useState([
-    { id: 1, task: "Tarea 1" },
-    { id: 2, task: "Tarea 2" },
-    { id: 3, task: "Tarea 3" },
-    { id: 4, task: "Tarea 4" },
-    { id: 5, task: "Tarea 5" },
-    { id: 6, task: "Tarea 6" },
-    { id: 7, task: "Tarea 7" },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const taskRef = useRef();
+
+  /* Permite almacenar los datos en el nabegador */
+/*   useEffect(() => {
+
+    const storedTodos = JSON.parse(localStorage.getItem(KEY));
+    if (storedTodos){
+        setTodos(storedTodos);
+    }
+}, [])
+
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(todos));
+  }, [todos]);
+ */
+  /* Fin de reglas */
 
   const agregarTarea = () => {
     const task = taskRef.current.value;
@@ -28,13 +37,65 @@ export function TodoList() {
       const newTask = {
         id: uuid(),
         task: task,
+        completed: false
       };
 
       return [...prevTodos, newTask];
     });
 
-    taskRef.current.value = "";
+    taskRef.current.value = null;
   };
+
+
+  const ResumenTareas = () => {
+    const cant = cantidadTareas()
+
+    if(cant === 0){
+      return (
+        <div className="alert alert-success mt-3">
+          Felicidades no tienes tareas pendientes :)
+        </div>
+      )
+    }
+
+    if(cant === 1){
+      return (
+        <div className="alert alert-info mt-3">
+          Te queda solamente una tarea pendiente!!
+        </div>
+      )
+    }
+
+    return (
+      <div className="alert alert-info mt-3">
+        Te quedan {cant} tareas pendientes!!
+      </div>
+    )
+
+  }
+
+  const cantidadTareas = () => {
+      return todos.filter((todo) => !todo.completed).length;
+  }
+
+  const cambiarEstadoTarea = (id) => {
+    console.log(id)
+    /* Tomamos los datos de la lista actuales */
+    const newTodos = [...todos]
+    /* Buscar el elemento con nuestro id */
+    const todo = newTodos.find((todo) => todo.id ===id)
+    /* Cambiamos el estado al caso contrario */
+    todo.completed = !todo.completed
+    /* Setear y actualizar la lista */
+    setTodos(newTodos)
+  }
+
+  const eliminarTareasCompletas = () => {
+    /* Se seleccionan todas las tareas que aun no se hacen */
+    const newTodos = todos.filter((todo) => !todo.completed)
+    setTodos(newTodos)
+ }
+
 
   return (
     <Fragment>
@@ -49,14 +110,20 @@ export function TodoList() {
         <button onClick={agregarTarea} className="btn btn-success ms-2">
           +
         </button>
+        <button onClick={eliminarTareasCompletas} className='btn btn-danger ms-2'>
+        <i class="bi bi-trash"></i>
+
+        </button>
       </div>
 
       <ul className="list-group">
         {/* Investigar que más se puede hacer con el método map */}
         {todos.map((todo) => (
-          <TodoItem todo={todo} key={todo.id}></TodoItem>
+          <TodoItem todo={todo} key={todo.id} cambiarEstado={cambiarEstadoTarea}></TodoItem>
         ))}
       </ul>
+
+      <ResumenTareas />
     </Fragment>
   );
 }
